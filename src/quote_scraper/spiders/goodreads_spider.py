9424da83649e53a8feb105f24bc2e1936b699b5b
Andy Xie <andyxie976@gmail.com>
2023-11-05 19:59:33 +0100

    scraping filter and allowed domains added,only tested in scrapy shell

--- src/quote_scraper/spiders/goodreads_spider.py
+++ src/quote_scraper/spiders/goodreads_spider.py
@@ -1,28 +1,28 @@
 from typing import Any, Final, Generator
 import re
-
 import scrapy
 from scrapy.http import Response
-
-from src.core.constants import QUOTE_NUM_LIKES
+from src.core.constants import QUOTE_NUM_LIKES, QUOTE_TEXT, QUOTE_AUTHOR, QUOTE_TAGS
 
 
 class GoodreadsSpider(scrapy.Spider):
     """Scraper to extract data from goodreads.com/quotes."""
 
     name = 'goodreads-spider'
+    allowed_domains = ['goodreads.com'] 
     start_urls = [
         'https://www.goodreads.com/quotes'
     ]
 
-    QUOTE_SELECTOR: Final[str] = 'dev.quote'
+    QUOTE_SELECTOR: Final[str] = 'div.quote'
     QUOTE_DETAILS: Final[str] = 'quoteDetails'
     QUOTE_FOOTER: Final[str] = 'quoteFooter'
     QUOTE_AVATAR: Final[str] = 'quoteAvatar'
     QUOTE_TEXT: Final[str] = 'quoteText'
     QUOTE_LIKES: Final[str] = 'a.smallText::text'
     QUOTE_AUTHOR_OR_TITLE: Final[str] = 'authorOrTitle'
-    NEXT_SELECTOR: Final[str] = ""  # TODO
+    QUOTE_TAGS: Final[str] = 'div.greyText.smallText.left'
+    NEXT_SELECTOR: Final[str] = "div.u-textAlignRight a.next_page"  # TODO
 
     NUM_LIKES_REGEX: Final[str] = r'\b\d+\b'
 
@@ -33,16 +33,22 @@ class GoodreadsSpider(scrapy.Spider):
         :param kwargs: additional kwargs
         :return:
         """
-        for quote in response.css(self.QUOTE_SELECTOR):
-            # TODO: yield quote
+        raw_quote_page = response.css(self.QUOTE_SELECTOR)
+
+        for quote in raw_quote_page:
+
             yield {
+                QUOTE_AUTHOR : quote.css(self.QUOTE_AUTHOR_OR_TITLE).get(),
+                QUOTE_TEXT : quote.css(self.QUOTE_TEXT).get(),
+                QUOTE_TAGS : quote.css(self.QUOTE_TAGS)[0].css('a::text').extract(),
+
                 # TODO: raise exception if multiple values are found for regex matching
                 # TODO: raise exception if result is not a a digit (i.e., string.isdigit() == False)
                 QUOTE_NUM_LIKES: int(re.findall(self.NUM_LIKES_REGEX, response.css(self.QUOTE_LIKES).get())[0])
             }
             pass
 
-        next_page = response.css(self.NEXT_SELECTOR).extract_first()
+        next_page = response.css(self.QUOTE_TAGS).attrib['href']
         if next_page:
             yield scrapy.Request(
                 response.urljoin(next_page)
