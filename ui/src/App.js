1da288f3c31b7bb9d3680cc8971219a2adebce8b
Ahsan Farooq <ahsanfarooq3423@gmail.com>
2023-11-06 20:28:04 +0100

    Initialize UI project with create-react-app and installed component library called material ui

---
+++ ui/src/App.js
@@ -0,0 +1,13 @@
+import { BasicButtons } from './components';
+
+function App() {
+  return (
+    <div>
+      <h1>Quotes Recommender (INITIAL SETUP) </h1>
+
+      <BasicButtons />
+    </div>
+  );
+}
+
+export default App;
