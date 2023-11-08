ad3e5ceeba070b6bd13610b7f4d5bcdf00b1b226
Henrik Fliegel <henrik.fliegel@finstreet.de>
2023-11-08 16:06:23 +0100

    spider for azquotes

---
+++ src/quotesscraper/quotesscraper/spiders/quotesspider.py
@@ -0,0 +1,38 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = "einstein"
+    
+    def start_requests(self):
+        url = "https://www.azquotes.com/quotes/authors/a/"
+        yield scrapy.Request(url = url, callback=self.parse_alphabet)
+        
+
+    def parse_alphabet(self, response):
+
+        alpabeth = response.css("ul.authors a::attr(href)").getall()
+        for url in alpabeth:
+            yield response.follow( url = "https://www.azquotes.com" + url, callback = self.parse_pop_authors)
+
+    def parse_pop_authors(self, response):
+
+        authors = response.css("div.profile.most-popular a::attr(href)").getall()
+
+        for url in authors:
+            yield response.follow( url = "https://www.azquotes.com" + url, callback = self.parse_author)
+
+
+
+    def parse_author(self, response):
+        for quote in response.css("div.wrap-block"):
+            yield {
+                "text": quote.css('a.title::text').get(),
+                "author": quote.css("a.title").attrib["data-author"],
+                "tags": quote.css("div.mytags a::text").getall(),
+                "likes": quote.css("a.heart24::text").getall()
+            }
+
+        next_page = "https://www.azquotes.com" + response.css("li.next a").attrib['href']
+        if next_page is not None:
+            yield response.follow(next_page, callback = self.parse_author)
+
