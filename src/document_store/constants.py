fda2b63ba2de02f8a944e76289726668cfd59932
Mathis <mathun3003@gmail.com>
2023-11-28 18:53:03 +0100

    PR Feedback

--- src/document_store/constants.py
+++ src/document_store/constants.py
@@ -2,6 +2,3 @@ from typing import Final
 
 # default settings
 DEFAULT_BATCH_SIZE: Final[int] = 500
-
-# max settings
-REDIS_MAX_RESULTS: Final[int] = 10000
--- src/utils/redis.py
+++ src/utils/redis.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from yarl import URL
@@ -9,11 +11,10 @@ class RedisConfig(BaseSettings):
     """Redis settings config"""
 
     model_config = SettingsConfigDict(env_prefix='REDIS_', env_file_encoding=TXT_ENCODING)
-    url: str = Field(default=None)
-    user: str = Field(default=None)
-    password: str = Field(min_length=0, default=None)
+    user: Optional[str] = Field(default=None)
+    password: Optional[str] = Field(min_length=0, default=None)
     host: str = Field(min_length=0, default='0.0.0.0')
-    port: int = Field(min_length=0, default='6379')
+    port: int = Field(default=6379)
     db: int = Field(ge=0, default=0)
 
     @property
@@ -22,7 +23,7 @@ class RedisConfig(BaseSettings):
         Returns yarl URL object of Redis string url.
         :return: yarl url object
         """
-        return URL(self.url)
+        return URL(f"redis://{self.host}:{self.port}")
 
     @property
     def redis_url_with_creds(self) -> URL:
@@ -30,6 +31,4 @@ class RedisConfig(BaseSettings):
         Returns yarl URL object of Redis string url with credentials
         :return: yarl URl object with credentials
         """
-        return (
-            self.redis_url.with_user(self.user).with_password(self.password).with_host(self.host).with_port(self.port)
-        )
+        return self.redis_url.with_user(self.user).with_password(self.password)
