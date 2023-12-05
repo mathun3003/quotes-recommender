c44ff347ba9eb2ffc9bcb894102a1cda1982c798
Henrik Fliegel <henrik.fliegel@finstreet.de>
2023-12-05 18:24:51 +0100

    add api to 'YOUR_BACKEND_ENDPOINT'

--- src/ui/src/components/searchbox/searchbox.jsx
+++ src/ui/src/components/searchbox/searchbox.jsx
@@ -45,75 +45,44 @@ const quoteFinderTags = [
   { title: 'PurposefulLiving', value: 'purposeful-living' },
 ];
 
-const mockResponseData = {
-  message: "success",
-  limit: 10,
-  count: 10,
-  data: [
-    {
-      id: 1,
-      text: "Be yourself; everyone else is already taken.",
-      author: "Oscar Wilde",
-      tags: ["be-yourself", "inspirational"],
-    },
-    {
-      id: 2,
-      text: "Be yourself; everyone else is already taken.",
-      author: "Oscar Wilde",
-      tags: ["be-yourself", "inspirational"],
-    },
-  ],
-  filters: ["inspirational"],
-  query: "be yourself"
-};
-
 export const SearchBox = () => {
   const [query, setQuery] = useState('');
   const [selectedTags, setSelectedTags] = useState([]);
   const [responseData, setResponseData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const limit = 10; // Dies können Sie als konstanten Wert behalten oder als State definieren, wenn es variabel sein soll
+  const limit = 10; 
 
+
+  
   const handleSearch = async () => {
     setIsLoading(true);
-    // Verzögern Sie die Ausführung, um das Verhalten einer Netzwerkanfrage zu simulieren
-    setTimeout(() => {
-      setResponseData(mockResponseData); // Verwenden Sie die Mock-Daten
-      setIsLoading(false);
-    }, 1000); // Verzögerung von 1000 Millisekunden (1 Sekunde)
+    try {
+      const requestBody = {
+        query,
+        filters: selectedTags.map(tag => tag.value),
+        limit: 10,
+        collection: '', 
+      };
+
+      const response = await fetch('YOUR_BACKEND_ENDPOINT', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestBody),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setResponseData(data);
+    } catch (error) {
+      console.error('There was an error!', error);
+    }
+    setIsLoading(false);
   };
-
-  // Ereignis-Handler für die Suche
- // const handleSearch = async () => {
- //   setIsLoading(true);
- //   try {
-      // Erstellen Sie hier Ihren requestBody mit den aktuellen Zuständen für query und selectedTags
-  //    const requestBody = {
-  //      query,
-  //      filters: selectedTags.map(tag => tag.value),
-  //      limit: 10,
-  //      collection: '', // Setzen oder entfernen Sie diese Eigenschaft je nach Bedarf
-  //    };
-
-   //   const response = await fetch('YOUR_BACKEND_ENDPOINT', {
-  //      method: 'POST',
-  //      headers: {
-  //        'Content-Type': 'application/json',
-  //      },
-  //      body: JSON.stringify(requestBody),
-   //   });
-
-  //    if (!response.ok) {
-   //     throw new Error(`HTTP error! status: ${response.status}`);
-  //    }
-
-  //    const data = await response.json();
-  //    setResponseData(data);
-  //  } catch (error) {
-  //    console.error('There was an error!', error);
-  //  }
-  //  setIsLoading(false);
-  //};
   return (
     <div className="search-box">
       <Stack spacing={2} alignItems="center">
