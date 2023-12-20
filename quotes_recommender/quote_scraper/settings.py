8a94d6f194543d662270e98e063b8e25346e2694
Andy Xie <andyxie976@gmail.com>
2023-12-20 17:25:44 +0100

    Item pipeline uncommented

--- quotes_recommender/quote_scraper/settings.py
+++ quotes_recommender/quote_scraper/settings.py
@@ -62,10 +62,10 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 # Low number indicates a high priority of the pipeline
-# ITEM_PIPELINES = {
-#     "quotes_recommender.quote_scraper.pipelines.GoodreadsToQdrantPipeline": 300,
-#     "quotes_recommender.quote_scraper.pipelines.AzquotesToQdrantPipeline": 300,
-# }
+ITEM_PIPELINES = {
+    "quotes_recommender.quote_scraper.pipelines.GoodreadsToQdrantPipeline": 300,
+    "quotes_recommender.quote_scraper.pipelines.AzquotesToQdrantPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
